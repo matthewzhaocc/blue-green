f33263d867e8ecf7903d97fb2a0220f47ed2f12b
Matthew Zhao <matthew@mail.matthew-cloud.com>
2021-01-17 14:15:15 -0800

    added a smol tool to switch env

---
+++ switchenv/go.mod
@@ -0,0 +1,8 @@
+module github.com/matthewzhaocc/blue-green/switch
+
+go 1.15
+
+require (
+	github.com/matthewzhaocc/common v0.0.0-20210117023421-723796742bcb
+	github.com/sirupsen/logrus v1.7.0
+)
---
+++ switchenv/switch.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+
+	"github.com/matthewzhaocc/common"
+	log "github.com/sirupsen/logrus"
+)
+
+const (
+	// GREEN the RED of the netflix
+	GREEN = "green"
+	// BLUE the BLACK of the netflix
+	BLUE = "blue"
+)
+// ApplyManifest applies the manifest of the environment
+func ApplyManifest(color string) error {
+	command := "apply -f " + color + "-env.yml"
+	cmd := exec.Command("kubectl", command)
+	return cmd.Run()
+}
+// FindEnvColor finds the color of the environment
+func FindEnvColor() string {
+	// thisis is a generic implementation based on environment variables, feel free to eidt it if you want to use a different method
+	return os.Getenv("CURRENT_ENV_COLOR")
+}
+func init() {
+	log.SetOutput(os.Stdout)
+}
+func main() {
+	if FindEnvColor() == GREEN {
+		// applying green environment manifest
+		log.Println("applying *blue* environment")
+		err := ApplyManifest(BLUE)
+		if common.CheckError(err) {
+			log.Println(err.Error())
+		}
+	} else if FindEnvColor() == BLUE{
+		log.Println("applying *green* environment")
+		err := ApplyManifest(GREEN)
+		if common.CheckError(err) {
+			log.Println(err.Error())
+		}
+	} else {
+		log.Println("Can't find the current environment colorz")
+	}
+	
+}
